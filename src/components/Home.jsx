5c8859ad49ee178691afbcf74913f9ab5a67cffc
luc4s-git <lucassantos4332@hotmail.com>
2024-01-03 15:04:41 -0300

    sidebar toggle

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,9 +1,13 @@
 import { useGlobalContext } from '../context/context';
+import { FaBars } from 'react-icons/fa';
 
 export default function Home() {
-  const { modalToggle } = useGlobalContext();
+  const { modalToggle, sidebarToggle } = useGlobalContext();
   return (
     <div>
+      <button className="menu" onClick={sidebarToggle}>
+        <FaBars className="bars" />
+      </button>
       <button className="btn" onClick={modalToggle}>
         Show Modal
       </button>
