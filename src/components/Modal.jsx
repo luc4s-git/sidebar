324d7f700c7d55dbeea334d9647fd241d4485deb
luc4s-git <lucassantos4332@hotmail.com>
2024-01-03 17:24:18 -0300

    modal component

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,3 +1,20 @@
+import { FaTimes } from 'react-icons/fa';
+import { useGlobalContext } from '../context/context';
+
 export default function Modal() {
-  return <div>Modal</div>;
+  const { isModalOpen, closeModal } = useGlobalContext();
+  return (
+    <>
+      {isModalOpen && (
+        <div className="modal-overlay">
+          <div className="modal-container">
+            <h4>Modal content</h4>
+            <button className="close-modal-btn" onClick={closeModal}>
+              <FaTimes />
+            </button>
+          </div>
+        </div>
+      )}
+    </>
+  );
 }
