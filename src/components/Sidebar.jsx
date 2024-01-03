76cfe9891b5ba874e873d643bdaa43ef6d0b0381
luc4s-git <lucassantos4332@hotmail.com>
2024-01-03 17:44:18 -0300

    sidebar component

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,3 +1,40 @@
+import { useGlobalContext } from '../context/context';
+
+import { socials, links } from '../data';
+import { FaTimes } from 'react-icons/fa';
+import logo from '../logo.svg';
+
 export default function Sidebar() {
-  return <div>Sidebar</div>;
+  const { isSidebarOpen, closeSidebar } = useGlobalContext();
+  return (
+    <aside className="sidebar">
+      <div className="sidebar-header">
+        <img src={logo} alt="logo" />
+        <button className="close-btn" onClick={closeSidebar}>
+          <FaTimes />
+        </button>
+      </div>
+      <ul className="links">
+        {links.map((link) => {
+          return (
+            <li key={link.id}>
+              <a href={link.url}>
+                {link.icon}
+                {link.text}
+              </a>
+            </li>
+          );
+        })}
+      </ul>
+      <ul className="social-links">
+        {socials.map((social) => {
+          return (
+            <li key={social.id}>
+              <a href={social.url}>{social.icon}</a>
+            </li>
+          );
+        })}
+      </ul>
+    </aside>
+  );
 }
--- src/data.jsx
+++ src/data.jsx
@@ -46,7 +46,7 @@ export const links = [
   },
 ];
 
-export const social = [
+export const socials = [
   {
     id: 1,
     url: 'https://www.twitter.com',
